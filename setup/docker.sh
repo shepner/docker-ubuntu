91f04e54e9194374b2be51fe36ca9727d1740e97
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2020-08-20 12:04:02 -0500

    Create docker.sh

---
+++ setup/docker.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+# Docker
+
+# Create docker service account
+sudo adduser --home /home/docker --uid 1003 --gid 1000 --shell /bin/bash docker
+
+sudo gpasswd -a docker sudo
+
+# Install Docker Engine on Ubuntu:
+
+# Update the apt package index and install packages to allow apt to use a repository over HTTPS:
+sudo apt-get update
+
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+# Add Docker’s official GPG key:
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+# Use the following command to set up the stable repository
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+# INSTALL DOCKER ENGINE
+# Update the apt package index, and install the latest version of Docker Engine and containerd, or go to the next step to install a specific version:
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+# Install Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod 755 /usr/local/bin/docker-compose
