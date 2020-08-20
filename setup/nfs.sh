7e3d463261f3bccdae06c4aaa8e6941eddfa1dd7
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2020-08-20 12:01:55 -0500

    Create nfs.sh

---
+++ setup/nfs.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# setup NFS
+
+sudo apt update
+sudo apt-get install -y nfs-common
+
+# Setup mounts
+
+sudo mkdir -p /mnt/nas/data1/docker
+echo "nas:/mnt/data1/docker /mnt/nas/data1/docker nfs rw 0 0" | sudo tee --append /etc/fstab
+sudo mkdir -p /mnt/nas/data2/docker
+echo "nas:/mnt/data2/docker /mnt/nas/data2/docker nfs rw 0 0" | sudo tee --append /etc/fstab
+
+sudo mkdir -p /mnt/nas/data1/media
+echo "nas:/mnt/data1/media /mnt/nas/data1/media nfs rw 0 0" | sudo tee --append /etc/fstab
+
+sudo mount -a
