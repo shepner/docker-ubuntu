8fa69ce2a4ddb5f78ef7dd07ad3b47c2fc0149b9
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2020-08-21 21:34:37 -0500

    Create sonarr.sh

---
+++ docker/downloads/sonarr.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# https://docs.linuxserver.io/images/docker-sonarr
+
+NAME=sonarr
+BASEDIR=/mnt/nas/data1/docker/$NAME
+
+sudo -u docker mkdir -p $BASEDIR/config
+#sudo -u docker mkdir -p $BASEDIR/tv
+#sudo -u docker mkdir -p $BASEDIR/downloads
+
+sudo docker service create \
+  --name $NAME \
+  --env PUID=1003 \
+  --env PGID=1000 \
+  --env TZ="America/Chicago" \
+  --publish published=8989,target=8989,protocol=tcp,mode=ingress \
+  --mount type=bind,src=/etc/localtime,dst=/etc/localtime,readonly=1 \
+  --mount type=bind,src=$BASEDIR/config,dst=/config \
+  --mount type=bind,src=/mnt/nas/data1/media/Videos,dst=/tv \
+  --mount type=bind,src=/mnt/nas/data1/docker/transmission/downloads/complete,dst=/downloads \
+  linuxserver/sonarr
+  
