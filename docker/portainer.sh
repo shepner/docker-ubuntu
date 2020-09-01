85be67c1e0e08f745a71592ef05f2f0938205291
Stephen Hepner <5170785+shepner@users.noreply.github.com>
2020-08-31 20:19:35 -0500

    Update portainer.sh

--- docker/portainer.sh
+++ docker/portainer.sh
@@ -18,8 +18,20 @@ sudo docker run --detach --restart=always \
   --name $NAME \
   --cpus=2 \
   --cpu-shares=1024 \
-  --publish published=8000,target=8000,protocol=tcp,mode=ingress \
-  --publish published=9000,target=9000,protocol=tcp,mode=ingress \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  -v portainer_data:/data \
+  --network=traefik_net \
+  --volume /var/run/docker.sock:/var/run/docker.sock \
+  --volume portainer_data:/data \
+  --label traefik.enable=true \
+  --label traefik.http.routers.portainer.entrypoints=web \
+  --label traefik.http.routers.portainer.rule=Host(portainer.asyla.org) \
   $IMAGE
+
+#sudo docker run --detach --restart=always \
+#  --name $NAME \
+#  --cpus=2 \
+#  --cpu-shares=1024 \
+#  --publish published=8000,target=8000,protocol=tcp,mode=ingress \
+#  --publish published=9000,target=9000,protocol=tcp,mode=ingress \
+#  --volume /var/run/docker.sock:/var/run/docker.sock \
+#  --volume portainer_data:/data \
+#  $IMAGE
